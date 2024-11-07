43266526b1d2633b807f049371704f964b29f69e
Obaddd <obadaljabberi@outlook.com>
2024-11-06 19:18:46 -0500

    Added home page

--- routes/index.js
+++ routes/index.js
@@ -17,21 +17,21 @@ router.get('/home', function(req, res, next) {
 
 /* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { 
+  res.render('aboutme', { 
     title: 'About Me',
      });
 });
 
 /* GET projects page. */
 router.get('/projects', function(req, res, next) {
-  res.render('index', { 
+  res.render('projects', { 
     title: 'Projects',
      });
 });
 
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { 
+  res.render('contact', { 
     title: 'Contact Me',
      });
 });
